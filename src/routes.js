e6e9137d0f34971448a0631e0968ee2789825cd2
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-01-01 10:48:53 -0300

    pagina carrinho de compras e listagem do que está dentro

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Categoria from "Pages/Categoria";
 import Home from "Pages/Home";
 import PaginaPadrao from "Pages/PaginaPadrao";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Carrinho from "Pages/Carrinho";
 
 export default function Router() {
     return(
@@ -10,6 +11,7 @@ export default function Router() {
                 <Route path='/' element={<PaginaPadrao/>}>
                     <Route index element={<Home/>}/>
                     <Route path='/categoria/:nomeCategoria' element={<Categoria/>}/>
+                    <Route path='/carrinho' element={<Carrinho/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
